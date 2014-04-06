1fc33e1dfe941336206849bb72d1c3b0f4cc0ea3
Mike Solomon <mike@mindsnacks.com>
2014-04-05 22:48:13 -0700

    Show Toast when text copied

--- Read My Photo/src/main/java/io/msol/readmyphoto/PhotoDetailFragment.java
+++ Read My Photo/src/main/java/io/msol/readmyphoto/PhotoDetailFragment.java
@@ -2,9 +2,12 @@ package io.msol.readmyphoto;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,11 +30,12 @@ import timber.log.Timber;
 
 public class PhotoDetailFragment extends Fragment {
     public static final String FILE_PATH = "FILE_PATH";
-    private static final long int MIN_TIME_BETWEEN_TOASTS = 1000;
+    private static final int MIN_TIME_BETWEEN_TOASTS = 1000;
 
     @Inject Tesseract tesseract;
     @Inject InputMethodManager inputMethodManager;
     @Inject ClipboardManager clipboardManager;
+    @Inject Context context;
 
     @InjectView(R.id.text) TextView text;
     @InjectView(R.id.progress) ProgressBar progress;
@@ -104,23 +108,31 @@ public class PhotoDetailFragment extends Fragment {
 
         ButterKnife.inject(this, getView());
         text.setOnFocusChangeListener(new KeyboardHidingFocusChangeListener());
+        text.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+            }
+
+            @Override public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+                clipAndNotifyWithDelay(s);
+            }
+
+            @Override public void afterTextChanged(final Editable s) {
+            }
+        });
     }
 
-    private void clipAndNotifyWithDelay(final String text) {
+    private void clipAndNotifyWithDelay(final CharSequence text) {
         clipboardManager.setPrimaryClip(ClipData.newPlainText("Text read from a photo", text));
 
         final Date now = new Date();
-        if (now.getTime() - lastToasted.getTime() < MIN_TIME_BETWEEN_TOASTS) {
+        final long millisecondsElapsed = now.getTime() - lastToasted.getTime();
+        if (millisecondsElapsed > MIN_TIME_BETWEEN_TOASTS) {
             lastToasted = now;
-            Toast.makeText(getActivity(), "Text copied!", Toast.LENGTH_SHORT);
-        } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override public void run() {
-                    clipAndNotifyWithDelay(text);
-                }
-            }, MIN_TIME_BETWEEN_TOASTS);
+            Toast toast = Toast.makeText(context, "Text copied!", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP, 0, 16);
+            toast.show();
         }
-    },
+    };
 
     private class KeyboardHidingFocusChangeListener implements View.OnFocusChangeListener {
         @Override public void onFocusChange(View v, boolean hasFocus){
